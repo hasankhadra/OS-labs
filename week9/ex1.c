77a99b738854046aa1afdc11b8d1f115cdef49ec
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2020-10-15 00:35:19 +0300

    Update ex1.c

--- week9/ex1.c
+++ week9/ex1.c
@@ -5,7 +5,7 @@
 const int frames1 = 10;
 const int frames2 = 50;
 const int frames3 = 100;
-const int max_bit = 30;
+const int max_bit = 31;
 
 int total, misses;
 int hit, lowest;
@@ -13,33 +13,41 @@ int hit, lowest;
 int main(){
     int ram[frames1], a[frames1];
     memset(ram, -1, sizeof(ram));
-    FILE* in = fopen("input.txt", "r");
+    memset(a, 0, sizeof(a));
+    FILE* in = fopen("C:\\Users\\prhas\\Desktop\\input.txt", "r");
     int input;
-
-    while(fscanf(in,"%d",&input) == 1){
+    fscanf (in, "%d", &input);
+    while(!feof(in)){
         hit = lowest = 0;
         total++;
-
         for(int i=0;i<frames1;i++){
-            a[i] >>= 1;
-
+            if (a[i] != -1){
+                a[i] /= 2;
+            }
             if(ram[i]==input){
                 a[i]|=(1<<(max_bit-1));
                 hit = 1;
             }
-            if(ram[i]==-1)
-                lowest=i;
-            else if(a[i]<a[lowest])
-                lowest=i;
+            if(ram[i]==-1){
+                if (ram[lowest] != -1){
+                    lowest=i;
+                }
+            }
+            else if(a[i]<a[lowest]){
+                if (ram[lowest] != -1){
+                    lowest=i;
+                }
+            }
         }
+        int tt = input;
+        fscanf (in, "%d", &input);
         if(hit)continue;
-
         ++misses;
         a[lowest]=(1<<(max_bit-1));
-        ram[lowest]=input;
+        ram[lowest]=tt;
     }
 
     printf("frames => %d\t in total => %d\t #hits => %d\t #misses => %d\n", frames1, total, total - misses, misses);
     printf("hits/miss => %.2f\n", (double)(total-misses)/(misses));
-
+    fclose(in);
 }
